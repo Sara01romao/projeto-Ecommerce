124f7c29f265853d2621ee9b64a9b5edf504d696
Sara Romao <sara-romao@live.com>
2021-03-06 00:02:52 -0300

    inicio

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ProdutoCard from './Componentes/produtos/Card'
+import './app.css'
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <ProdutoCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componentes/produtos/Card.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import './Card.css'
+
+import productsData from '../../products.json';
+
+
+const productsList = productsData.product;
+  
+  class ProdutoCard extends Component{
+    render(){
+      return(
+        <div className="cards">
+          {productsData.map((produto)=>{
+            return(
+              <div className="cardInfo" key={produto.id}>
+            
+                    
+                    <div className="card">
+                                <h1>{produto.name}</h1>
+                                <img src={`/assets/${produto.image}`}/>
+                                <p>Score: {produto.score}</p>
+                            
+                                <div className="infoCard">
+                                    <h3>Preço R$ {produto.price} </h3>
+                                    <button><img src="/assets/cart-icon.svg"></img>Add carrinho</button>
+                                </div>
+                          
+                             </div>
+                  
+                        
+                    
+              </div>
+            );
+          })
+        }
+        
+        </div>
+        
+      );
+    }
+  }
+
+    
+  
+    
+export default ProdutoCard;
+
+
+
+
+
+
+// const ProdutoCard =() =>(
+    
+//       <div className="ListaProdutos">
+        
+//         <div className="card">
+//             <h1>Nome</h1>
+//             <img src="/assets/super-mario-odyssey.png"></img>
+//             <p>Score: 250</p>
+           
+//            <div className="infoCard">
+//                 <h3>Preço R$00,00 </h3>
+//                 <button><img src="/assets/cart-icon.svg"></img>Add carrinho</button>
+//            </div>
+        
+//         </div>
+//     </div>
+// );
+
